0757a3946e83be45677528e23ea657d36d396536
Alisha <mahmoodalisha35@gmail.com>
2025-06-11 13:48:49 +0530

    added checks

--- server.js
+++ server.js
@@ -59,7 +59,8 @@ app.use((err, req, res, next) => {
     )
 }); */
 
-app.listen(5000, () => {
-    //connect();
-    console.log('Server is running on port 5000');
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
 });
+
